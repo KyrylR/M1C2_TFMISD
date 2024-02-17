eb79d42843b323e669ac49ed8b16f81b09438b13
Kyryl R <kyryl.ryabov@gmail.com>
2024-02-17 17:10:19 +0200

    Added main files for lab2

---
+++ lab2/include/cli.h
@@ -0,0 +1,7 @@
+#ifndef CLI_H
+#define CLI_H
+
+// Initializes and runs the CLI for the addition and multiplication programs.
+void runCLI();
+
+#endif // CLI_H
---
+++ lab2/src/cli.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void runCLI() {
+    printf("Welcome to the addition and multiplication program!\n");
+}
